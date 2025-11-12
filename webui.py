39bad5402fee336a8733e9625f6b77b46058ebaf
chirag249 <chiraggowdadu004@gmail.com>
2025-11-12 20:59:45 +0530

    u

--- webui.py
+++ webui.py
@@ -729,4 +729,4 @@ def serve_frontend(path):
         return send_from_directory(frontend_dir, 'index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
